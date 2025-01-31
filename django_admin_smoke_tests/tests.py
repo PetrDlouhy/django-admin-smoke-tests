a792c9d1b55112e437f459f9c1988215c47af43b
Petr Dlouhý <petr.dlouhy@email.cz>
2025-01-31 10:47:35 +0100

    fix texts

--- django_admin_smoke_tests/tests.py
+++ django_admin_smoke_tests/tests.py
@@ -145,8 +145,8 @@ class AdminSiteSmokeTestMixin(AssertElementMixin):
                     missing_apps.append(app)
             if missing_apps:
                 warnings.warn(
-                    "Not all apps in only_apps were found, "
-                    f"using only apps {missing_apps}",
+                    "Not all apps in only_apps were found: "
+                    f" {missing_apps}",
                     UserWarning,
                 )
 
@@ -158,8 +158,8 @@ class AdminSiteSmokeTestMixin(AssertElementMixin):
                     missing_modeladmins.append(model_admin)
             if missing_modeladmins:
                 warnings.warn(
-                    "Not all modeladmins in only_modeladmins were found, "
-                    f"using only modeladmin {missing_modeladmins}",
+                    "Not all modeladmins in only_modeladmins were found: "
+                    f" {missing_modeladmins}",
                     UserWarning,
                 )
 
